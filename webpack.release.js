f6fd0209f93fe68fb19377e538de06e1d78b5bf3
Mark Holmes <smlmrkhlms@gmail.com>
2016-04-03 12:25:45 -0700

    Build & Deploy

--- webpack.release.js
+++ webpack.release.js
@@ -22,7 +22,7 @@ const config = {
   output: {
     filename: '[name].[hash].js',
     path: './dist/release/',
-    publicPath: '/dist/release/'
+    publicPath: '/'
   },
   plugins: [
     new extractTextPlugin('[name].[hash].css'),
